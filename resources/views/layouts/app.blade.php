b6e21d797f68fc896485bf9fcb4b37b0e325ba5e
tuantt <tran.thanh.tuan269@gmail.com>
2018-07-24 00:10:14 +0700

    css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'TOMATO') }}</title>
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('public/css/all.css') }}" rel="stylesheet">

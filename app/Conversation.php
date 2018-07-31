facc0957ba160db0c74e3ebf8d5286ca4f464d0d
tuantt <tran.thanh.tuan269@gmail.com>
2018-07-31 21:44:21 +0700

    add notification to chat

--- app/Conversation.php
+++ app/Conversation.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Conversation extends Model
 {
-    protected $fillable = ['message', 'user_id', 'group_id', 'type', 'conversation'];
+    protected $fillable = ['message', 'user_id', 'group_id', 'type', 'conversation', 'update_by'];
 
     public function user()
     {
